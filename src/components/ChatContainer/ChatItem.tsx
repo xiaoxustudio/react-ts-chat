0d32acbd166b194881dcf52957816077100213b2
xiaoxustudio <1783558957@qq.com>
2024-12-25 11:42:45 +0800

    修改聊天容器布局

--- src/components/ChatContainer/ChatItem.tsx
+++ src/components/ChatContainer/ChatItem.tsx
@@ -1,132 +1,129 @@
-import { Avatar, Flex, Image, Popover } from "antd";
-import classNames from "classnames";
-import { forwardRef, useContext, useState } from "react";
-import { ServerUrl } from "@/consts";
-import { Content } from "antd/es/layout/layout";
-import useUserStore from "@/store/useUserStore";
-import { MoreOutlined, UserOutlined } from "@ant-design/icons";
-import ChatContext from "./utils/ChatContext";
-import { ChatItemType, type ChatItemData } from "./type";
-import style from "./chat-item.module.less";
+import { Avatar, Flex, Image, Popover } from 'antd';
+import classNames from 'classnames';
+import { forwardRef, useContext, useState } from 'react';
+import { ServerUrl } from '@/consts';
+import { Content } from 'antd/es/layout/layout';
+import useUserStore from '@/store/useUserStore';
+import { MoreOutlined, UserOutlined } from '@ant-design/icons';
+import ChatContext from './utils/ChatContext';
+import { ChatItemType, type ChatItemData } from './type';
+import style from './chat-item.module.less';
+import TextArea from 'antd/es/input/TextArea';
 
 interface ChatItemProps {
-	item: ChatItemData;
-	index: number;
+    item: ChatItemData;
+    index: number;
 }
 interface MenuItemDataProps {
-	target: string;
-	index: number;
+    target: string;
+    index: number;
 }
 
 function MenuItemData({ target, index }: MenuItemDataProps) {
-	const { onWidthDraw } = useContext(ChatContext);
-	return (
-		<Flex vertical>
-			<Content
-				className={style.menuBtn}
-				onClick={() => onWidthDraw?.(target, index)}
-			>
-				撤回
-			</Content>
-		</Flex>
-	);
+    const { onWidthDraw } = useContext(ChatContext);
+    return (
+        <Flex vertical>
+            <Content className={style.menuBtn} onClick={() => onWidthDraw?.(target, index)}>
+                撤回
+            </Content>
+        </Flex>
+    );
 }
 
 const ChatItem = forwardRef<HTMLDivElement, ChatItemProps>((props, ref) => {
-	const { item, index, ...reset } = props;
-	const { username } = useUserStore();
-	const [_, setHover] = useState(false);
+    const { item, index, ...reset } = props;
+    const { username } = useUserStore();
+    const [_, setHover] = useState(false);
 
-	return (
-		<Flex
-			ref={ref}
-			onMouseEnter={() => setHover(true)}
-			onMouseLeave={() => setHover(false)}
-			justify={item.send_id === username ? "right" : "left"}
-			className={classNames(style.ChatItemBox)}
-			vertical
-			{...reset}
-		>
-			{(+item.type as ChatItemType) === ChatItemType.System && (
-				<>
-					<Flex className={style.ChatItemBack}>{item.content}</Flex>
-				</>
-			)}
-			{(+item.type as ChatItemType) === ChatItemType.Common && (
-				<>
-					{/* 用户名称 */}
-					<Flex
-						className={style.NickName}
-						justify={item.send_id === username ? "right" : "left"}
-					>
-						{item.send_id !== username && (
-							<Avatar
-								size="large"
-								icon={!item.avatar && <UserOutlined />}
-								src={`${ServerUrl}${item.avatar?.slice(1)}`}
-							/>
-						)}
-						{item.nickname}
-						{item.send_id === username && (
-							<Avatar
-								size="large"
-								icon={!item.avatar && <UserOutlined />}
-								src={`${ServerUrl}${item.avatar?.slice(1)}`}
-							/>
-						)}
-					</Flex>
-					{/* 内容 */}
-					<Flex
-						className={style.Content}
-						justify={item.send_id === username ? "right" : "left"}
-					>
-						{item.content}
-					</Flex>
-					{/* 附件 */}
-					<Flex justify={item.send_id === username ? "right" : "left"}>
-						{item.files.map((val) => (
-							<Image
-								key={val}
-								width={50}
-								height={50}
-								src={`${ServerUrl}/${val}`}
-							/>
-						))}
-					</Flex>
-					{item.files.length > 0 && (
-						<Flex
-							justify={item.send_id === username ? "right" : "left"}
-							style={{ fontSize: "10px", color: "#ccc" }}
-						>
-							携带文件数：{item.files.length}
-						</Flex>
-					)}
-					{/* 时间 */}
-					<Flex
-						className={style.Time}
-						justify={item.send_id === username ? "right" : "left"}
-					>
-						{item.send_id === username && (
-							<Popover
-								trigger="hover"
-								placement="left"
-								content={
-									<MenuItemData target={item.receive_id} index={index} />
-								}
-							>
-								<MoreOutlined className={style.menuBtn} />
-							</Popover>
-						)}
-						{item.time}
-						{item.send_id !== username && (
-							<Popover trigger="hover" placement="right">
-								<MoreOutlined className={style.menuBtn} />
-							</Popover>
-						)}
-					</Flex>
-				</>
-			)}
-		</Flex>
-	);
+    return (
+        <Flex
+            ref={ref}
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            justify={item.send_id === username ? 'right' : 'left'}
+            className={classNames(style.ChatItemBox)}
+            vertical
+            {...reset}
+        >
+            {(+item.type as ChatItemType) === ChatItemType.System && (
+                <>
+                    <Flex className={style.ChatItemBack}>{item.content}</Flex>
+                </>
+            )}
+            {(+item.type as ChatItemType) === ChatItemType.Common && (
+                <>
+                    {/* 用户名称 */}
+                    <Flex
+                        className={style.NickName}
+                        justify={item.send_id === username ? 'right' : 'left'}
+                    >
+                        {item.send_id !== username && (
+                            <Avatar
+                                size="large"
+                                icon={!item.avatar && <UserOutlined />}
+                                src={`${ServerUrl}${item.avatar?.slice(1)}`}
+                            />
+                        )}
+                        {item.nickname}
+                        {item.send_id === username && (
+                            <Avatar
+                                size="large"
+                                icon={!item.avatar && <UserOutlined />}
+                                src={`${ServerUrl}${item.avatar?.slice(1)}`}
+                            />
+                        )}
+                    </Flex>
+                    {/* 内容 */}
+                    <Flex
+                        className={`${style.Content} justify-between`}
+                        justify={item.send_id === username ? 'right' : 'left'}
+                    >
+                        <Content>{''}</Content>
+                        <TextArea
+                            className="inline-block !max-w-[640px] border-none text-right hover:shadow-none"
+                            value={item.content}
+                            autoSize
+                            readOnly
+                        ></TextArea>
+                    </Flex>
+                    {/* 附件 */}
+                    <Flex justify={item.send_id === username ? 'right' : 'left'}>
+                        {item.files.map((val) => (
+                            <Image key={val} width={50} height={50} src={`${ServerUrl}/${val}`} />
+                        ))}
+                    </Flex>
+                    {item.files.length > 0 && (
+                        <Flex
+                            justify={item.send_id === username ? 'right' : 'left'}
+                            style={{ fontSize: '10px', color: '#ccc' }}
+                        >
+                            携带文件数：{item.files.length}
+                        </Flex>
+                    )}
+                    {/* 时间 */}
+                    <Flex
+                        className={style.Time}
+                        justify={item.send_id === username ? 'right' : 'left'}
+                    >
+                        {item.send_id === username && (
+                            <Popover
+                                trigger="hover"
+                                placement="left"
+                                content={<MenuItemData target={item.receive_id} index={index} />}
+                            >
+                                <MoreOutlined className={style.menuBtn} />
+                            </Popover>
+                        )}
+                        {item.time}
+                        {item.send_id !== username && (
+                            <Popover trigger="hover" placement="right">
+                                <MoreOutlined className={style.menuBtn} />
+                            </Popover>
+                        )}
+                    </Flex>
+                </>
+            )}
+        </Flex>
+    );
 });
 export default ChatItem;
