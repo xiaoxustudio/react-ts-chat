fad5b753c7fcfa5a3ceac97126016a825f1b2249
xiaoxustudio <1783558957@qq.com>
2024-12-29 18:06:04 +0800

    消除react router警告

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,5 @@ import router from './router';
 import './styles/tailwind.css';
 import './styles/app.less';
 createRoot(document.getElementById('root')!).render(
-    <RouterProvider router={router}></RouterProvider>,
+    <RouterProvider router={router} future={{ v7_startTransition: true }}></RouterProvider>,
 );
