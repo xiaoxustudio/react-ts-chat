c11edaaaf5a6333afb0310defce7009b29d6a69c
xiaoxustudio <1783558957@qq.com>
2024-12-30 18:40:52 +0800

    ...

--- src/pages/token/Token.tsx
+++ src/pages/token/Token.tsx
@@ -1,21 +1,33 @@
-import { Button, Flex, Layout } from 'antd';
-import { useMemo } from 'react';
+import { Button, Flex, Layout, message } from 'antd';
+import { useEffect, useMemo } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
-import { layoutStyle, headerStyle, contentStyle } from '@/consts';
+import { layoutStyle, headerStyle, contentStyle, RepCode } from '@/consts';
 import { Header, Content } from 'antd/es/layout/layout';
 import classNames from 'classnames';
 import style from './token.module.less';
+import ValidEmail from '@/apis/user/valid-email';
 
 function TokenVerify() {
     const [params] = useSearchParams();
     const navigate = useNavigate();
-    const handleBack = () => navigate(-1);
+    const handleBack = () => navigate('/');
     const token_id = useMemo(() => (params ? params.get('token') : ''), [params]);
+    const user_id = useMemo(() => (params ? params.get('user') : ''), [params]);
+    useEffect(() => {
+        ValidEmail({ code: token_id!, user: user_id! }).then((data) => {
+            if (data.code === RepCode.Success) {
+                message.success(data.msg);
+            } else {
+                message.error(data.msg);
+            }
+            handleBack();
+        });
+    }, []); //eslint-disable-line
     return (
         <Flex gap="middle" wrap>
             <Layout style={layoutStyle}>
                 <Header style={headerStyle}>
-                    <Button onClick={handleBack}>返回上一页</Button>
+                    <Button onClick={handleBack}>返回</Button>
                 </Header>
                 <Content style={contentStyle}>
                     <Flex className={classNames(style.TokenBox, 'justify-center')}>
